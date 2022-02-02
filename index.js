16c9cc0c4fba5a89565dd5396699cd81239bf062
Fredrik Johansson <fredrik.johansson@tobii.com>
2022-02-02 13:40:04 +0100

    Export correctly and define __dirname

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 import {spawn} from 'child_process';
 import {join} from 'path';
 
-function sayHello() {
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export const sayHello = function() {
   return new Promise((resolve, _reject) => {
     const hello = spawn(join(__dirname, './bin/Hello'));
     hello.stdout.on('data', (data) => resolve(`${data}`));
   });
 }
 
-export default {
-  sayHello
-};
+export default sayHello;
