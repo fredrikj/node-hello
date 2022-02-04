85b9d4d8a1d59a52f8a755349187dc9962b7d877
Fredrik Johansson <fredrik.johansson@tobii.com>
2022-02-04 15:22:23 +0100

    Differ on platforms

--- getHello.js
+++ getHello.js
@@ -1,5 +1,6 @@
 import fetch from 'node-fetch';
 import fs from 'fs/promises';
+import {filename} from './index.js';
 
 async function getHelloVersion() {
   const packagedata = JSON.parse(await fs.readFile('./package.json'));
@@ -9,11 +10,12 @@ async function getHelloVersion() {
 async function getArtifact() {
   const version = await getHelloVersion();
   const url =
-    `https://github.com/fredrikj/hello/releases/download/${version}/HelloDarwin`;
+    'https://github.com/fredrikj/hello/releases/download/'
+    +  `${version}/${filename}`;
   console.log('I GET', url);
   const response = await fetch(url);
   const data = await response.arrayBuffer();
-  const path = './bin/Hello';
+  const path = `./bin/${filename}`;
   await fs.writeFile(path, Buffer.from(data));
   await fs.chmod(path, '755');
 }
--- index.js
+++ index.js
@@ -7,9 +7,17 @@ import { dirname } from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+const suffix = {
+  win32: 'win32.exe',
+  darwin: 'darwin',
+  linux: 'linux'
+}
+
+export const filename = `Hello-${suffix[process.platform]}`;
+
 export const sayHello = function() {
   return new Promise((resolve, _reject) => {
-    const hello = spawn(join(__dirname, './bin/Hello'));
+    const hello = spawn(join(__dirname, `./bin/${filename}`));
     hello.stdout.on('data', (data) => resolve(`${data}`));
   });
 }
