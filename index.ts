4914e978e61c9ceac389b69820f8b64b2ae3cebd
Fredrik Johansson <fredrik.johansson@tobii.com>
2022-02-09 13:45:47 +0100

    Error on fail to start and give last line on error

--- index.ts
+++ index.ts
@@ -7,15 +7,22 @@ import {filename} from './constants';
 export const runExecutable = function(args: string[] = []): Observable<number> {
   return new Observable((subscriber: Subscriber<number>) => {
     const child = spawn(join(__dirname, `./bin/${filename}`), args);
+    child.on('error', (err) => {
+      subscriber.error(err);
+    });
     child.stdout.on('data', (data) => {
       const progress = parseInt(data.toString(), 10);
       subscriber.next(progress);
+    });
+    let stderrLastLine = '';
+    child.stderr.on('data', (data) => {
+      stderrLastLine = data.toString();
     });
     child.on('close', (code) => {
       if (code === 0) {
         subscriber.complete();
       } else {
-        subscriber.error(code);
+        subscriber.error(stderrLastLine);
       }
     });
     const cleanupLogic: TeardownLogic = function() {
